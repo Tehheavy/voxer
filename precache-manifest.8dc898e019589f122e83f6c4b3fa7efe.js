beef09cb235b5cb5799753acdc12771903b8d54a
alexdushevski <39350701+Tehheavy@users.noreply.github.com>
2019-10-26 21:48:40 +0300

    Updates

---
+++ precache-manifest.8dc898e019589f122e83f6c4b3fa7efe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "850f2532c4450142317dd49b27dab17f",
+    "url": "/voxer/index.html"
+  },
+  {
+    "revision": "3e19f95b07d6c40cc4eb",
+    "url": "/voxer/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "fb8a9f05c46576392a31",
+    "url": "/voxer/static/css/main.01983370.chunk.css"
+  },
+  {
+    "revision": "3e19f95b07d6c40cc4eb",
+    "url": "/voxer/static/js/2.9b8a9804.chunk.js"
+  },
+  {
+    "revision": "fb8a9f05c46576392a31",
+    "url": "/voxer/static/js/main.0afbf697.chunk.js"
+  },
+  {
+    "revision": "726392971d782996e32c",
+    "url": "/voxer/static/js/runtime-main.620b3ac4.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/voxer/static/media/logo.25bf045c.svg"
+  }
+]);
